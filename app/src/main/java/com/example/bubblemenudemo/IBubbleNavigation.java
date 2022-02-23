c0a725574c477112255a2ebfc13a82052be25713
SonTV-TP <sonkoii1995@gmail.com>
2022-02-23 16:49:10 +0700

    init pj

---
+++ app/src/main/java/com/example/bubblemenudemo/IBubbleNavigation.java
@@ -0,0 +1,16 @@
+package com.example.bubblemenudemo;
+
+import android.graphics.Typeface;
+
+@SuppressWarnings("unused")
+public interface IBubbleNavigation {
+    void setNavigationChangeListener(BubbleNavigationChangeListener navigationChangeListener);
+
+    void setTypeface(Typeface typeface);
+
+    int getCurrentActiveItemPosition();
+
+    void setCurrentActiveItem(int position);
+
+    void setBadgeValue(int position, String value);
+}
---
+++ app/src/main/java/com/example/bubblemenudemo/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bubblemenudemo
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // setContentView(R.layout.activity_splash)
+    }
+}
